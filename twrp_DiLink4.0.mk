bd4d177f2eca310660f8115bd1f4efd46ed7b114
abdmeh11 <30433370+abdmeh11@users.noreply.github.com>
2025-08-05 19:44:46 -0700

    Update twrp_DiLink4.0.mk

--- twrp_DiLink4.0.mk
+++ twrp_DiLink4.0.mk
@@ -4,26 +4,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from DiLink4.0 device
 $(call inherit-product, device/byd/DiLink4.0/device.mk)
-
 PRODUCT_DEVICE := DiLink4.0
 PRODUCT_NAME := twrp_DiLink4.0
 PRODUCT_BRAND := BYD-AUTO
 PRODUCT_MODEL := DiLink4.0 For BYD AUTO
 PRODUCT_MANUFACTURER := byd
-
 PRODUCT_GMS_CLIENTID_BASE := android-byd
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lito-user 10 QKQ1.210218.001 build10060113 release-keys"
-
 BUILD_FINGERPRINT := qti/lito/lito:10/QKQ1.210218.001/build10060113:user/release-keys
