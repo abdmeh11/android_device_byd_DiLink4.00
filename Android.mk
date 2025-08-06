78caf8f5fa220119b70a38488be1e6600011d824
abdmeh11 <30433370+abdmeh11@users.noreply.github.com>
2025-08-05 18:45:17 -0700

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for BYD-AUTO DiLink4.0 For BYD AUTO (DiLink4.0)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),DiLink4.0)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for BYD-AUTO DiLink4.0 For BYD AUTO (DiLink4.0)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_DiLink4.0-user
+add_lunch_combo omni_DiLink4.0-userdebug
+add_lunch_combo omni_DiLink4.0-eng
